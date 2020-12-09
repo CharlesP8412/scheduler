b4ed664e4522f4ff27174c3056afbdefd2294aa9
Charles Prystupa <Charles.Prystupa@gmail.com>
2020-12-09 17:21:38 -0500

    Integrate w. Api to Fetch Days, Appointments, and Interviewers

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,48 +3,8 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
+import {getAppointmentsForDay} from "helpers/selectors"
 import "components/Application.scss";
-
-
-const appointmentsData = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Fred Mack",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-];
 
 
 export default function Application(props) {
@@ -53,29 +13,24 @@ export default function Application(props) {
     days: [],
     appointments: {}
   })
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const parsedAppts = dailyAppointments.map(appt => <Appointment key={appt.id} {...appt} />)
+  // const parsedAppts = appointments.map(appt => <Appointment key={appt.id} {...appt} />)
 
-const dailyAppointments = [];
-const parsedAppts = dailyAppointments.map(appt => <Appointment key={appt.id} {...appt} />)
-// const parsedAppts = appointments.map(appt => <Appointment key={appt.id} {...appt} />)
-  
   const setDay = day => setState({ ...state, day });
-  
-  //Prev mess gets rid of linting warning of using setState in useEffects... somehow...
-  // Previous items in object day and appts held in place w. prev?
-  const setDays = days => setState(prev => ({ ...prev, days }));
-  // const setDays = days => setState({ ...state, days });
-
+  // const setDays = days => setState(prev => ({ ...prev, days }));
   //Fetch and Set Days
   useEffect(() => {
-    axios.get(`http://localhost:8001/api/days`)
-      .then(response => { 
-        setDays(response.data)
-      })
-      .catch(error => console.log(error))
+    Promise.all([
+      axios.get(`http://localhost:8001/api/days`),
+      axios.get(`http://localhost:8001/api/appointments`),
+      axios.get(`http://localhost:8001/api/interviewers`)
+    ])
+      .then((all) => {
+        console.log(all[1].data)
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
+      });
   }, []);
-
-  
-
 
   return (
     <main className="layout">
