4d9922460e4ddf452425d1be9f1db0fff5e78e85
Charles Prystupa <Charles.Prystupa@gmail.com>
2020-12-10 14:38:24 -0500

    useVisualMode Hook Complete: Move between modes with a history for moving back

---
+++ src/hooks/useVisualMode.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+
+  function transition(newMode, replaceLast = false) {
+    setMode(newMode)
+
+    if (replaceLast) {
+      setHistory(prev => {
+        prev.pop();
+        return [...prev, newMode]
+      })
+    } else {
+      setHistory(prev => {
+        return [...prev, newMode]
+      })
+    }
+  }
+
+  function back() {
+    //Pop off latest in history[] (but not directly and not if only one)
+    setHistory(prev => {
+      if (prev.length > 1) {
+        prev.pop()
+        setHistory(prev)
+      }
+      //Set Mode to the new last entry in array
+      setMode(history[history.length - 1])
+    })
+
+  }
+  return { mode, transition, back };
+}
+
+
+
