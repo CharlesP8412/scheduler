4752ff813e21b2076fa7ffadf5fb39cb9516b49e
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-23 15:58:19 -0500

    Rmv Env Import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import axios from "axios";
-
-if (process.env.REACT_APP_API_BASE_URL) {
-  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-}
-
 
 import "index.scss";
 
